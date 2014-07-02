aa397e0fef529d6ddfd309104cfe0f26108920f9
Marcel <redstyleca@hotmail.de>
2014-07-02 12:38:15 +0200

    Semikolons entfernt und den Code an die Guideline angepasst.

--- core/client/classes/RPC.lua
+++ core/client/classes/RPC.lua
@@ -1,10 +1,10 @@
 RPC = inherit(Singleton)
 
 function RPC:constructor()
-	self.tFunctions = {};
+	self.m_Functions = {}
 	
-	addEvent('onServerRPC', true);
-	addEventHandler('onServerRPC', resourceRoot, bind(RPC.handleRequests, self));
+	addEvent('onServerRPC', true)
+	addEventHandler('onServerRPC', resourceRoot, bind(RPC.handleRequests, self))
 end
 
 function RPC:destructor()
@@ -12,61 +12,58 @@ function RPC:destructor()
 end
 
 function RPC:registerFunction(funcname, callback)
-	assert(type(funcname) == 'string', 'Argument provided to funcname is not a string');
-	assert(type(callback) == 'function', 'Argument provided to callback is not a function');
-	assert(_G[callback], 'Callback is not declared');
-	
-	if(not self.tFunctions[funcname]) then
-		self.tFunctions[funcname] = callback;
+	assert(type(funcname) == 'string' and type(callback) == 'function', 'Bad Argument @RPC.registerFunction')
+	if not self.m_Functions[funcname] then
+		self.m_Functions[funcname] = callback
 	end	
 end
 
 function RPC:removeFunction(funcname)
-	assert(type(funcname) == 'string', 'Argument provided to funcname is not a string');
+	assert(type(funcname) == 'string', 'Bad Argument @RPC.removeFunction')
 	
-	if(self.tFunctions[funcname]) then
-		self.tFunctions[funcname] = nil;
+	if self.m_Functions[funcname] then
+		self.m_Functions[funcname] = nil
 	end
 end
 
 function RPC:handleRequests(data)
-	assert(type(data) == 'table', 'Bad data provided');
+	assert(type(data) == 'table', 'Bad Argument @RPC.handleRequests')
 	
 	if(#data >= 1) then
-		local funcname = data[1];
+		local funcname = data[1]
 		
-		if(self.tFunctions[funcname]) then
-			local tmp = {};
+		if(self.m_Functions[funcname]) then
+			local tmp = {}
 			
 			for index, arg in pairs(data) do
-				if(index ~= 1) then
-					tmp[index - 1] = arg;
+				if index ~= 1 then
+					tmp[index - 1] = arg
 				end
 			end
 			
-			self.tFunctions[funcname](unpack(tmp));
+			self.m_Functions[funcname](unpack(tmp))
 		end
 	end
 end
 
 function RPC:callFunction(funcname, ...)
-	assert(type(funcname) == 'string', 'Bad Argument provied to funcname');
-	if(self.tFunctions[funcname]) then
-		self.tFunctions[funcname](...);
+	assert(type(funcname) == 'string', 'Bad Argument @RPC.callFunction')
+	if(self.m_Functions[funcname]) then
+		self.m_Functions[funcname](...)
 	end
 end
 
 function RPC:send(funcname, ...)
-	assert(type(funcname) == 'string');
+	assert(type(funcname) == 'string', 'Bad Argument @RPC.send')
 
-	local tmp = {};
-	tmp[1] = funcname;
-	if(#{...} >= 1) then
+	local tmp = {}
+	tmp[1] = funcname
+	if #{...} >= 1 then
 		for index, arg in pairs({...}) do
-			tmp[index + 1] = arg;
+			tmp[index + 1] = arg
 		end
 	end
 	
-	triggerServerEvent('onClientRPC', resourceRoot, tmp);
+	triggerServerEvent('onClientRPC', resourceRoot, tmp)
 end
 
--- core/server/classes/RPC.lua
+++ core/server/classes/RPC.lua
@@ -1,10 +1,10 @@
 RPC = inherit(Singleton)
 
 function RPC:constructor()
-	self.tFunctions = {};
+	self.m_Functions = {}
 	
-	addEvent('onClientRPC', true);
-	addEventHandler('onClientRPC', resourceRoot, bind(RPC.handleRequests, self));
+	addEvent('onClientRPC', true)
+	addEventHandler('onClientRPC', resourceRoot, bind(RPC.handleRequests, self))
 end
 
 function RPC:destructor()
@@ -12,62 +12,61 @@ function RPC:destructor()
 end
 
 function RPC:registerFunction(funcname, callback)
-	assert(type(funcname) == 'string', 'Argument provided to funcname is not a string');
-	assert(type(callback) == 'function', 'Argument provided to callback is not a function');
-	assert(_G[callback], 'Callback is not declared');
+	assert(type(funcname) == 'string' and type(callback) == 'function', 'Bad Argument @RPC.registerFunction')
 	
-	if(not self.tFunctions[funcname]) then
-		self.tFunctions[funcname] = callback;
+	if not self.m_Functions[funcname] then
+		self.m_Functions[funcname] = callback
 	end	
 end
 
 function RPC:removeFunction(funcname)
-	assert(type(funcname) == 'string', 'Argument provided to funcname is not a string');
+	assert(type(funcname) == 'string', 'Bad Argument @RPC.removeFunction')
 	
-	if(self.tFunctions[funcname]) then
-		self.tFunctions[funcname] = nil;
+	if self.m_Functions[funcname] then
+		self.m_Functions[funcname] = nil
 	end
 end
 
 function RPC:handleRequests(data)
-	assert(type(data) == 'table', 'Bad data provided');
+	assert(type(data) == 'table', 'Bad Argument @RPC.handleRequests')
 	
 	if(#data >= 1) then
-		local funcname = data[1];
+		local funcname = data[1]
 		
-		if(self.tFunctions[funcname]) then
-			local tmp = {};
+		if self.m_Functions[funcname] then
+			local tmp = {}
 			
 			for index, arg in pairs(data) do
-				if(index ~= 1) then
-					tmp[index - 1] = arg;
+				if index ~= 1 then
+					tmp[index - 1] = arg
 				end
 			end
 			
-			self.tFunctions[funcname](unpack(tmp));
+			self.m_Functions[funcname](unpack(tmp))
 		end
 	end
 end
 
 function RPC:callFunction(funcname, ...)
-	assert(type(funcname) == 'string', 'Bad Argument provied to funcname');
-	if(self.tFunctions[funcname]) then
-		self.tFunctions[funcname](...);
+	assert(type(funcname) == 'string', 'Bad Argument @RPC.callFunction')
+	if self.m_Functions[funcname] then
+		self.m_Functions[funcname](...)
 	end
 end
 
 function RPC:send(player, funcname, ...)
-	assert(getElementType(player) == 'player');
-	assert(type(funcname) == 'string');
-
-	local tmp = {};
-	tmp[1] = funcname;
-	if(#{...} >= 1) then
-		for index, arg in pairs({...}) do
-			tmp[index + 1] = arg;
-		end
-	end
+	assert(getElementType(player) == 'player' and type(funcname) == 'string', 'Bad Argument @RPC.send')
 	
-	triggerClientEvent(player, 'onServerRPC', resourceRoot, tmp);
+	if self.m_Functions[funcname] then
+		local tmp = {}
+		tmp[1] = funcname
+		if #{...} >= 1 then
+			for index, arg in pairs({...}) do
+				tmp[index + 1] = arg
+			end
+		end
+		
+		triggerClientEvent(player, 'onServerRPC', resourceRoot, tmp)
+	end
 end
 
